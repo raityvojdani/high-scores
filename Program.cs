6afa96e1ae14e360aa2b797b3634813ad9553bf5
Emertat <Emertat@RAITY>
2024-09-29 13:52:28 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace high_scores;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        // Create a list of sample scores
+        List<int> scores = new List<int> { 10, 30, 90, 30, 100, 20, 10, 0, 30, 40, 40, 70, 70 };
+
+        // Initialize the HighScores object with the list of scores
+        HighScores highScores = new HighScores(scores);
+
+        // Accessing the methods of HighScores class
+        Console.WriteLine("All Scores: " + string.Join(", ", highScores.Scores()));
+        Console.WriteLine("Latest Score: " + highScores.Latest());
+        Console.WriteLine("Personal Best: " + highScores.PersonalBest());
+        Console.WriteLine("Top Three Scores: " + string.Join(", ", highScores.PersonalTopThree()));
+
+    }
+}
---
+++ high-scores.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace high_scores;
+public class HighScores
+{
+    private List<int> _scores;
+    private List<int> _topThree= new List<int>();
+
+    public HighScores(List<int> list)
+    {
+        _scores = list;
+    }
+
+    public List<int> Scores()
+    {
+        return _scores;
+    }
+
+    public int Latest()
+    {
+        return _scores[_scores.Count-1];
+    }
+
+    public int PersonalBest()
+    {
+      return  _scores.Max();
+    }
+
+    public List<int> PersonalTopThree()
+    {
+        
+        _topThree = _scores.OrderByDescending(score => score).Take(3).ToList();
+        return _topThree;
+    }
+}
